8234069fa651955680261442925a20227791d22b
a <a@aui-MacBook-Pro.local>
2019-12-23 18:23:36 +0900

    dev init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-const a = 'chicken';
+const Koa = require('koa');
+
+const app = new Koa();
+app.use((ctx, next) => {
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401;
+    return;
+  }
+  next().then(() => {
+    console.log('END');
+  });
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log(3);
+  next();
+});
+
+app.use(ctx => {
+  ctx.body = 'hello world....!';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
